bf1688fb2aca26bd50427b98c5df5518124d0cbd
Alex <113412134+Alexmdu58@users.noreply.github.com>
2022-11-06 20:22:59 +0530

    week5.1

---
+++ Week-5/week5.1/week5.1.js
@@ -0,0 +1,36 @@
+var promiseHelper = {
+  message : "not defined",
+  log: function() { 
+       console.log(this.message)
+  },
+};
+
+function* generator(resolveFn, rejectFn) {
+  doTask = (message) => {
+    resolveFn(message);
+  };
+
+  try {
+    console.log("in Generator");
+     doTask(yield);// "Do Task 1"
+     doTask(yield);
+     doTask(yield);
+     console.log("Generator ends");
+  } catch (er) {
+    rejectFn(er.message);
+  }
+}
+
+const resolve =  (message) => promiseHelper.log.bind({message})();
+const reject =  (message) => promiseHelper.log.bind({message})();
+
+
+let obj = generator(resolve, reject);
+console.log("start");
+obj.next();
+console.log("first next");
+setTimeout(() => {  obj.next("Do Task 1"); }, 1000);
+setTimeout(() => {  obj.next("Do Task 2"); }, 2000);
+setTimeout(() => {  obj.throw(new Error("Rejected...")); }, 3000);
+setTimeout(() => {  obj.next("This wont work..."); }, 4000);
+console.log("End...");
